bb24ee1fa7cc73c63f8e962831f423bed9e07906
Caleb Smith <karagenit@outlook.com>
2018-01-17 17:22:53 -0500

    Add Drivetrain Body

--- src/com/techhounds/subsystems/Drivetrain.java
+++ src/com/techhounds/subsystems/Drivetrain.java
@@ -1,18 +1,49 @@
 package com.techhounds.subsystems;
 
+import com.techhounds.RobotMap;
+import com.techhounds.commands.DriverControl;
+
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Drivetrain extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	
+	private Spark motorLeft;
+	private Spark motorRight;
+	
+	public Drivetrain() {
+		motorLeft = new Spark(RobotMap.DRIVE_MOTOR_LEFT);
+		motorRight = new Spark(RobotMap.DRIVE_MOTOR_RIGHT);
+	}
+	
+	/**
+	 * Set the drivetrain motor power.
+	 * 
+	 * @param right Decimal value [-1.0, 1] to set
+	 * @param left Decimal value [-1.0, 1] to set
+	 */
+	public void setMotors(double right, double left) {
+		left = constrain(left);
+		right = constrain(right);
+		motorLeft.set(left);
+		motorRight.set(right);
+	}
+	
+	/**
+	 * Limits the given value to -1, 1
+	 * 
+	 * @param value
+	 * @return
+	 */
+	private double constrain(double value) {
+		return Math.min(Math.max(value, -1), 1);
+	}
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new DriverControl());
     }
 }
 

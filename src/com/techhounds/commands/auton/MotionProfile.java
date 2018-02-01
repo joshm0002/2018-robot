ec867d9a350f05634d5abf9534732bcd16686e71
armaangoel78 <armaangoel78@gmail.com>
2018-01-31 23:06:57 -0500

    Added code translation of sin acceleration motion profile

--- src/com/techhounds/commands/auton/MotionProfile.java
+++ src/com/techhounds/commands/auton/MotionProfile.java
@@ -2,6 +2,9 @@ package com.techhounds.commands.auton;
 
 import java.io.File;
 
+import com.techhounds.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -23,23 +26,55 @@ public class MotionProfile extends Command {
 			return new File(filename);
 		}
 	}
+	
+	private int maxAccel, distance;
+	private long startTime;
 
-    public MotionProfile(Profile profile) {
+	/**
+	 * 
+	 * @param profile
+	 * @param maxAccel in milliseconds
+	 * @param distance in meters
+	 */
+    public MotionProfile(Profile profile, int maxAccel, int distance) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    		this.maxAccel = maxAccel;
+    		this.distance = distance;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    		startTime = System.currentTimeMillis();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    		//constants from https://www.chiefdelphi.com/media/papers/3107
+    		double t = Math.sqrt((2*Math.PI*distance)/maxAccel);
+    		double k1 = (2*Math.PI)/t;
+    		double k2 = maxAccel/k1;
+    		// time passed since inizialize
+    		long timePassed = startTime - System.currentTimeMillis();
+    		// velocity calculation
+    		double powerRight = k2*(1-Math.sin(k1*timePassed));
+    		double powerLeft = powerRight;
+    		Robot.drivetrain.setMotors(powerRight, powerLeft);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    		// constants from https://www.chiefdelphi.com/media/papers/3107
+    		double t = Math.sqrt((2*Math.PI*distance)/maxAccel);
+		double k1 = (2*Math.PI)/t;
+		double k2 = maxAccel/k1;
+		double k3 = 1/k1;
+		// time passed sinze inizialize
+		long timePassed = startTime - System.currentTimeMillis();
+		// displacement calculation
+    		double distance_travelled = k2*(timePassed-k3*Math.sin(k1*timePassed));
+    		
+        return distance_travelled >= distance;
     }
 
     // Called once after isFinished returns true

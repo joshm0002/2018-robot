baa58284ac052c08c4a6c7f2ec6397b53918023e
Caleb Smith <karagenit@outlook.com>
2018-02-02 17:21:19 -0500

    Add Missing TrajectoryPointPair File

---
+++ src/com/techhounds/commands/auton/TrajectoryPointPair.java
@@ -0,0 +1,19 @@
+package com.techhounds.commands.auton;
+
+import com.ctre.phoenix.motion.TrajectoryPoint;
+
+/**
+ * This class represents a pair of TrajectoryPoint (part of the
+ * Talon Phoenix library) - one for the left side and one for
+ * the right side of the drivetrain.
+ */
+public class TrajectoryPointPair {
+	
+	public final TrajectoryPoint right;
+	public final TrajectoryPoint left;
+	
+	public TrajectoryPointPair(TrajectoryPoint right, TrajectoryPoint left) {
+		this.right = right;
+		this.left = left;
+	}
+}

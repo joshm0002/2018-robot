957f2c5314517d9bdd8e2341d7a94ac006e6f438
Andrew Gostomelsky <andrewgostomelsky@gmail.com>
2018-02-05 15:38:40 -0500

    Discount Double Check

--- src/com/techhounds/subsystems/Intake.java
+++ src/com/techhounds/subsystems/Intake.java
@@ -1,18 +1,29 @@
 package com.techhounds.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.techhounds.RobotMap;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
 public class Intake extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	
+	private TalonSRX intake;
+	private TalonSRX intakeAngler;
+	
+	public Intake() {
+		intake = new WPI_TalonSRX(RobotMap.MOTOR_INTAKE);
+	}
+	
+	public void setPower(double power){
+		intake.set(ControlMode.PercentOutput, check(power));
+	}
+	
+    public double check(double in){
+    	return Math.min(Math.max(in, -1), 1);
+    }
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        
     }
 }
-

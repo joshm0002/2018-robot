600dfbb358365e96a217c1f52c2c93463786bc2c
Caleb Smith <karagenit@outlook.com>
2018-02-21 11:49:00 -0500

    Add Profile Recorder

---
+++ src/com/techhounds/commands/ProfileRecorder.java
@@ -0,0 +1,87 @@
+package com.techhounds.commands;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.techhounds.Robot;
+
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class ProfileRecorder extends Command {
+	
+	private ProfileRecorderRunnable recorder;
+	private Notifier notifier;
+	private double period;
+
+    public ProfileRecorder(double period) {
+    	recorder = new ProfileRecorderRunnable();
+    	notifier = new Notifier(recorder);
+    	this.period = period;
+    	SmartDashboard.putString("Profile Recorder File Name", "Test");
+    }
+
+    protected void initialize() {
+    	notifier.startPeriodic(period);
+    }
+
+    protected void execute() {
+    	// Intentionally blank: the Notifier records the data
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	notifier.stop();
+    	recorder.writeToFile(SmartDashboard.getString("Profile Recorder File Name", "Test"));
+    }
+
+    protected void interrupted() {}
+    
+    public class ProfileRecorderRunnable implements Runnable {
+    	
+    	/**
+    	 * SCHEMA:
+    	 * { RIGHT_DIST, RIGHT_VEL, LEFT_DIST, LEFT_VEL }
+    	 */
+    	List<double[]> data = new ArrayList<double[]>();
+
+		@Override
+		public void run() {
+			double[] point = new double[4];
+			point[0] = Robot.drivetrain.getRightDistance();
+			point[1] = Robot.drivetrain.getRightVelocity();
+			point[2] = Robot.drivetrain.getLeftDistance();
+			point[3] = Robot.drivetrain.getLeftVelocity();
+			data.add(point);
+		}
+		
+		public void writeToFile(String filename) {
+			try {
+				BufferedWriter file = new BufferedWriter(new FileWriter(filename + ".csv"));
+				
+				file.write(filename + "\n" + data.size() + "\n");
+				
+				for (double[] point : data) {
+					// FIXME: this format is not correct, we need to use the 254 standard
+					file.write("" + point[0] + ", " + point[1] + ", " + point[2] + ", " + point[3] + "\n");
+				}
+				
+				file.flush();
+				file.close();
+			} catch (IOException e) {
+				System.err.println("Failed to write Recorded Profile to " + filename);
+			}
+		}
+    	
+    }
+}

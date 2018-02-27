4fd71cfb2de15ea25e33cdf3a0b326bae621c60a
Caleb Smith <karagenit@outlook.com>
2018-02-27 18:38:17 -0500

    Fix Missing Commands

--- src/com/techhounds/auton/paths/StraightSwitch.java
+++ src/com/techhounds/auton/paths/StraightSwitch.java
@@ -2,7 +2,8 @@ package com.techhounds.auton.paths;
 
 import com.techhounds.auton.util.DelayedCommand;
 import com.techhounds.intake.SetIntakePower;
-import com.techhounds.powerpack.basic.SetElevatorMiddle;
+import com.techhounds.powerpack.SetElevatorPosition;
+import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -12,7 +13,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class StraightSwitch extends CommandGroup {
 
     public StraightSwitch() {
-    	addParallel(new DelayedCommand(new SetElevatorMiddle(), 1));
+    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SWITCH), 1));
     	addSequential(new DriveDistance(110, 3));
     	addSequential(new SetIntakePower(-1), 3);
     }

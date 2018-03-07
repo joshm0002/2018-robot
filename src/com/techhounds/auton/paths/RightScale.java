e91a433538dee311de4193729ccaa6e66c51546e
Caleb Smith <karagenit@outlook.com>
2018-03-07 10:44:16 -0500

    Cleanup Scale Auton

--- src/com/techhounds/auton/paths/RightScale.java
+++ src/com/techhounds/auton/paths/RightScale.java
@@ -1,6 +1,5 @@
 package com.techhounds.auton.paths;
 
-import com.techhounds.auton.util.DelayedCommand;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
 import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
@@ -20,8 +19,8 @@ public class RightScale extends CommandGroup {
     	addSequential(new DriveDistance(270, 270, 0.4, 0.4 * 0.95)); // less power left, curves left
     	addSequential(new SetElevatorPosition(ElevatorPosition.SCALE));
     	addParallel(new SetTiltPosition(TiltPosition.DOWN));
-    	addSequential(new DelayedCommand(new SetIntakePower(-1), 0.5), 3);
     	addSequential(new WaitCommand(1));
+    	addSequential(new SetIntakePower(-0.75), 1);
     	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
     }
 }

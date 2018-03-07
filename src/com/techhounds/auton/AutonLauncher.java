c7a6cabb0b0c530be83a4ccda7901b2f982dcfdb
Caleb Smith <karagenit@outlook.com>
2018-03-07 11:13:10 -0500

    Update Auton Launcher

--- src/com/techhounds/auton/AutonLauncher.java
+++ src/com/techhounds/auton/AutonLauncher.java
@@ -1,7 +1,12 @@
 package com.techhounds.auton;
 
+import com.techhounds.auton.FieldState.Position;
 import com.techhounds.auton.paths.Baseline;
+import com.techhounds.auton.paths.CenterLeftSwitch;
+import com.techhounds.auton.paths.CenterRightSwitch;
 import com.techhounds.auton.paths.LeftScale;
+import com.techhounds.auton.paths.LeftSwitch;
+import com.techhounds.auton.paths.RightSwitch;
 import com.techhounds.auton.paths.StraightSwitch;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -17,27 +22,33 @@ public class AutonLauncher {
 	
 	enum Auton {
 		BASELINE,
-		STRAIGHT_SWITCH,
-		STRAIGHT_SCALE
+		FRONT_SWITCH,
+		FRONT_SCALE,
+		SIDE_SWITCH,
+		SIDE_SCALE // NOT DONE
 	}
 	
 	public static final SendableChooser<Auton> autonChoices = new SendableChooser<>();
 
 	public static void addChoices() {
 		autonChoices.addDefault("Baseline", Auton.BASELINE);
-		autonChoices.addObject("Straight Switch", Auton.STRAIGHT_SWITCH);
-		autonChoices.addObject("Straight Scale", Auton.STRAIGHT_SCALE);
+		autonChoices.addObject("Straight Switch", Auton.FRONT_SWITCH);
+		autonChoices.addObject("Straight Scale", Auton.FRONT_SCALE);
+		autonChoices.addObject("Side Switch", Auton.SIDE_SWITCH);
 		SmartDashboard.putData("Auton Chooser", autonChoices);
 	}
 	
 	public static void runAuton(FieldState field) {
 		System.out.println("Running Auton " + autonChoices.getSelected().toString());
 		switch(autonChoices.getSelected()) {
-		case STRAIGHT_SWITCH:
-			runStraightSwitch(field);
+		case FRONT_SWITCH:
+			runFrontSwitch(field);
 			break;
-		case STRAIGHT_SCALE:
-			runStraightScale(field);
+		case FRONT_SCALE:
+			runFrontScale(field);
+			break;
+		case SIDE_SWITCH:
+			runSideSwitch(field);
 			break;
 		case BASELINE:
 			runBaseline();
@@ -52,15 +63,23 @@ public class AutonLauncher {
 		new Baseline().start();
 	}
 	
-	public static void runStraightSwitch(FieldState field) {
-		if (field.getRobotPosition() == field.getSwitchPosition()) {
+	public static void runFrontSwitch(FieldState field) {
+		if (field.getRobotPosition() == Position.Middle) { // start in center, do either
+			if (field.getSwitchPosition() == Position.Right) {
+				new CenterRightSwitch().start();
+			} else if (field.getSwitchPosition() == Position.Left) {
+				new CenterLeftSwitch().start();
+			} else {
+				runBaseline();
+			}
+		} else if (field.getRobotPosition() == field.getSwitchPosition()) { //start on one, do it if it's ahead of us
 			new StraightSwitch().start();
 		} else {
 			runBaseline();
 		}
 	}
 	
-	public static void runStraightScale(FieldState field) {
+	public static void runFrontScale(FieldState field) {
 		if (field.getRobotPosition() == field.getScalePosition()) {
 			new LeftScale().start();
 		} else {
@@ -68,31 +87,41 @@ public class AutonLauncher {
 		}
 	}
 	
-	public static void runSwitch(FieldState field) {
-		
+	public static void runSideSwitch(FieldState field) {
+		if (field.getRobotPosition() == field.getSwitchPosition()) {
+			if (field.getSwitchPosition() == Position.Right) {
+				new RightSwitch().start();
+			} else if (field.getSwitchPosition() == Position.Left) {
+				new LeftSwitch().start();
+			} else {
+				runBaseline();
+			}
+		} else {
+			runBaseline();
+		}
 	}
 	
-	public static void runScale(FieldState field) {
-		
-	}
-	
-	public static void runSwitchOrScale(FieldState field) {
-		
-	}
-	
-	public static void runScaleOrSwitch(FieldState field) {
-		
-	}
-	
-	public static void runScaleSwitch(FieldState field) {
-		
-	}
-	
-	public static void runScaleSwitchSwitch(FieldState field) {
-		
-	}
-	
-	public static void runScaleSwitchScale(FieldState field) {
-		
-	}
+//	public static void runSideScale(FieldState field) {
+//		
+//	}
+//	
+//	public static void runSwitchOrScale(FieldState field) {
+//		
+//	}
+//	
+//	public static void runScaleOrSwitch(FieldState field) {
+//		
+//	}
+//	
+//	public static void runScaleSwitch(FieldState field) {
+//		
+//	}
+//	
+//	public static void runScaleSwitchSwitch(FieldState field) {
+//		
+//	}
+//	
+//	public static void runScaleSwitchScale(FieldState field) {
+//		
+//	}
 }

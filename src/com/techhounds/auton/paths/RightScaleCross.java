10629ea0dfb9b84b75772646ee349e9f0bdf6a4c
Caleb Smith <karagenit@outlook.com>
2018-03-19 11:56:32 -0400

    Cleanup Cross Scale Autons

--- src/com/techhounds/auton/paths/RightScaleCross.java
+++ src/com/techhounds/auton/paths/RightScaleCross.java
@@ -1,5 +1,6 @@
 package com.techhounds.auton.paths;
 
+import com.techhounds.auton.util.DelayedCommand;
 import com.techhounds.auton.util.DriveStraight;
 import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.SetIntakePower;
@@ -9,7 +10,6 @@ import com.techhounds.tilt.SetTiltPosition;
 import com.techhounds.tilt.Tilt;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -19,16 +19,20 @@ public class RightScaleCross extends CommandGroup {
     public RightScaleCross() {
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	
+    	// drive across
     	addSequential(new DriveStraight(218, 0.5), 8);
     	addSequential(new TurnToAngleGyro(85), 3);
+    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 4));
+    	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 4));
     	addSequential(new DriveStraight(184, 0.5), 8);
     	
-    	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
-    	addParallel(new SetTiltPosition(Tilt.POS_MID));
+    	// put in scale
     	addSequential(new TurnToAngleGyro(-10), 2);
-    	
-    	addSequential(new WaitCommand(1));
-    	addSequential(new DriveStraight(18, 0.4), 1);
+    	addSequential(new DriveStraight(18, 0.4), 2);
     	addSequential(new SetIntakePower(-0.5), 1);
+    	
+    	// back off
+    	addSequential(new DriveStraight(-20, -0.4));
+    	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT));
     }
 }

0817983aba65a952a7f97829b0abfcd9e609e788
Caleb Smith <karagenit@outlook.com>
2018-03-19 11:34:42 -0400

    Cleanup Double Scale Auton

--- src/com/techhounds/auton/paths/RightScaleScale.java
+++ src/com/techhounds/auton/paths/RightScaleScale.java
@@ -1,14 +1,13 @@
 package com.techhounds.auton.paths;
 
-import com.techhounds.auton.util.DelayedCommand;
-import com.techhounds.auton.util.DriveArc;
+import com.techhounds.arm.GrabCube;
 import com.techhounds.auton.util.DriveStraight;
 import com.techhounds.auton.util.TurnByAngleGyro;
+import com.techhounds.intake.IntakeUntilDetected;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
 import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
 import com.techhounds.tilt.SetTiltPosition;
-import com.techhounds.tilt.SetTiltPosition.TiltPosition;
 import com.techhounds.tilt.Tilt;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -21,29 +20,11 @@ public class RightScaleScale extends CommandGroup {
 
     public RightScaleScale() {
     	// do scale
-//    	addSequential(new RightScale());
-       	// Set tilt/elevator
-    	addParallel(new SetTiltPosition(TiltPosition.MIDDLE));    	
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 2.5));
-
-    	// drive up & curve
-    	addSequential(new DriveStraight(230, 0.6), 6);
-    	addSequential(new DriveArc(30, 10, 0.4, 0.2), 2); // curve left
-    	
-    	// eject the cube
-    	addParallel(new SetTiltPosition(TiltPosition.MIDDLE)); //TODO down?
-//    	addSequential(new WaitCommand(1));
-    	addSequential(new SetIntakePower(-0.5), 1);
-    	
-    	// back off and reset
-    	addSequential(new DriveArc(-20), 1);
-    	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
-    	
-    	// END RIGHT SCALE
+    	addSequential(new RightScale());
     	
     	addSequential(new TurnByAngleGyro(-90), 2);
-//    	addParallel(new GrabCube(), 3);
-    	addParallel(new SetIntakePower(1), 3);
+    	addParallel(new GrabCube(), 3);
+    	addParallel(new IntakeUntilDetected(), 3);
     	addSequential(new DriveStraight(24, 0.4), 3);
     	
     	// place in scale

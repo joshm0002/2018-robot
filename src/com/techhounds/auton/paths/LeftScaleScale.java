d42e4b692b7a25e5b17b3f0988f392a22bfafc62
Caleb Smith <karagenit@outlook.com>
2018-03-24 11:37:30 -0400

    Update Double Cube Autons

--- src/com/techhounds/auton/paths/LeftScaleScale.java
+++ src/com/techhounds/auton/paths/LeftScaleScale.java
@@ -1,16 +1,21 @@
 package com.techhounds.auton.paths;
 
 import com.techhounds.arm.GrabCube;
+import com.techhounds.auton.util.DelayedCommand;
+import com.techhounds.auton.util.DriveArc;
 import com.techhounds.auton.util.DriveStraight;
 import com.techhounds.auton.util.TurnByAngleGyro;
+import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.IntakeUntilDetected;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
 import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
 import com.techhounds.tilt.SetTiltPosition;
 import com.techhounds.tilt.Tilt;
+import com.techhounds.tilt.SetTiltPosition.TiltPosition;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -18,23 +23,42 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class LeftScaleScale extends CommandGroup {
 
     public LeftScaleScale() {
-    	// do scale
-    	addSequential(new LeftScale());
+    	// Set tilt/elevator
+    	addParallel(new SetTiltPosition(TiltPosition.DOWN));    	
+    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1.5));
+
+    	// drive up & curve
+    	addSequential(new DriveStraight(230, 0.6), 6);
+    	addSequential(new DriveStraight(10, 0.4), 1);
+    	addSequential(new DriveArc(10, 30, 0.2, 0.4), 2); // curve right
+//    	addSequential(new TurnToAngleGyro(-45), 1.5);
     	
-    	addSequential(new TurnByAngleGyro(90), 2);
-    	addParallel(new GrabCube(), 3);
-    	addParallel(new IntakeUntilDetected(), 3);
-    	addSequential(new DriveStraight(24, 0.4), 3);
+    	// eject the cube
+    	addParallel(new SetTiltPosition(TiltPosition.MIDDLE));
+    	addSequential(new SetIntakePower(-0.4), 1);
+    	
+    	// back off and reset
+    	addSequential(new DriveStraight(-20, -0.35), 2);
+    	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
+    	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT), 3);
+    	
+    	// END RIGHT SCALE
+    	
+    	addSequential(new TurnToAngleGyro(115), 2);
+    	addParallel(new GrabCube(), 4);
+    	addParallel(new IntakeUntilDetected(), 4);
+    	addSequential(new DriveStraight(85, 0.4), 4);
+    	addSequential(new WaitCommand(0.5));
     	
     	// place in scale
-    	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
+    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1));
     	addParallel(new SetTiltPosition(Tilt.POS_MID));
-    	addSequential(new DriveStraight(-24, 0.4), 2);
-    	addSequential(new TurnByAngleGyro(-90), 2);
+    	addSequential(new DriveStraight(-85, -0.5), 3);
+    	addSequential(new TurnByAngleGyro(-55), 2);
     	addSequential(new DriveStraight(20, 0.4), 2);
     	addSequential(new SetIntakePower(-0.75), 1);
     	
-    	// back off
+//    	// back off
     	addSequential(new DriveStraight(-24, -0.4), 2);
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT), 2);
--- src/com/techhounds/auton/paths/RightScaleScale.java
+++ src/com/techhounds/auton/paths/RightScaleScale.java
@@ -2,7 +2,9 @@ package com.techhounds.auton.paths;
 
 import com.techhounds.arm.GrabCube;
 import com.techhounds.auton.util.DelayedCommand;
+import com.techhounds.auton.util.DriveArc;
 import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.TurnByAngleGyro;
 import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.IntakeUntilDetected;
 import com.techhounds.intake.SetIntakePower;
@@ -21,9 +23,6 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class RightScaleScale extends CommandGroup {
 
     public RightScaleScale() {
-    	// do scale
-//    	addSequential(new RightScale(), );
-    	
        	// Set tilt/elevator
     	addParallel(new SetTiltPosition(TiltPosition.DOWN));    	
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1.5));
@@ -31,8 +30,8 @@ public class RightScaleScale extends CommandGroup {
     	// drive up & curve
     	addSequential(new DriveStraight(230, 0.6), 6);
     	addSequential(new DriveStraight(10, 0.4), 1);
-//    	addSequential(new DriveArc(30, 10, 0.4, 0.2), 2); // curve left
-    	addSequential(new TurnToAngleGyro(-45), 1.5);
+    	addSequential(new DriveArc(30, 10, 0.4, 0.2), 2); // curve left
+//    	addSequential(new TurnToAngleGyro(-45), 1.5);
     	
     	// eject the cube
     	addParallel(new SetTiltPosition(TiltPosition.MIDDLE));
@@ -55,13 +54,13 @@ public class RightScaleScale extends CommandGroup {
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1));
     	addParallel(new SetTiltPosition(Tilt.POS_MID));
     	addSequential(new DriveStraight(-85, -0.5), 3);
-//    	addSequential(new TurnByAngleGyro(55), 2);
-//    	addSequential(new DriveStraight(20, 0.4), 2);
-//    	addSequential(new SetIntakePower(-0.75), 1);
-//    	
+    	addSequential(new TurnByAngleGyro(55), 2);
+    	addSequential(new DriveStraight(20, 0.4), 2);
+    	addSequential(new SetIntakePower(-0.75), 1);
+    	
 //    	// back off
-//    	addSequential(new DriveStraight(-24, -0.4), 2);
-//    	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
-//    	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT), 2);
+    	addSequential(new DriveStraight(-24, -0.4), 2);
+    	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
+    	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT), 2);
     }
 }

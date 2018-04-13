df18d3b4c5b7bd7041c2912a178cf82dad58ae12
Caleb Smith <karagenit@outlook.com>
2018-04-13 08:12:51 -0400

    Add Right Side Scale Auton from Side

---
+++ src/com/techhounds/auton/paths/RightScaleSide.java
@@ -0,0 +1,24 @@
+package com.techhounds.auton.paths;
+
+import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.TurnToAngleGyro;
+import com.techhounds.intake.SetIntakePower;
+import com.techhounds.powerpack.SetElevatorPosition;
+import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class RightScaleSide extends CommandGroup {
+
+    public RightScaleSide() {
+    	addSequential(new DriveStraight(260, 0.7), 5);
+    	addSequential(new DriveStraight(30, 0.4), 2);
+    	addSequential(new TurnToAngleGyro(-90), 2);
+    	addSequential(new DriveStraight(-40, -0.4), 2);
+    	addSequential(new SetElevatorPosition(ElevatorPosition.SCALE), 5);
+    	addSequential(new SetIntakePower(-0.6), 1);
+    }
+}

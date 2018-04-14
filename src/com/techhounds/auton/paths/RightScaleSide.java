aa6c34e6cb73adcbf1e8e0adee499b4ccbd29871
Caleb Smith <karagenit@outlook.com>
2018-04-14 08:24:48 -0400

    Tune Side Auton

--- src/com/techhounds/auton/paths/RightScaleSide.java
+++ src/com/techhounds/auton/paths/RightScaleSide.java
@@ -5,6 +5,8 @@ import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
 import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
+import com.techhounds.tilt.SetTiltPosition;
+import com.techhounds.tilt.Tilt;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -14,11 +16,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RightScaleSide extends CommandGroup {
 
     public RightScaleSide() {
-    	addSequential(new DriveStraight(260, 0.7), 5);
+    	addSequential(new DriveStraight(275, 0.7), 5);
     	addSequential(new DriveStraight(30, 0.4), 2);
     	addSequential(new TurnToAngleGyro(-90), 2);
-    	addSequential(new DriveStraight(-40, -0.4), 2);
+    	addSequential(new DriveStraight(-20, -0.4), 2);
     	addSequential(new SetElevatorPosition(ElevatorPosition.SCALE), 5);
+    	addSequential(new DriveStraight(25, 0.3), 2);
+    	addParallel(new SetTiltPosition(Tilt.POS_MID));
     	addSequential(new SetIntakePower(-0.6), 1);
     }
 }
